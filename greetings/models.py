7889a56615e31536fe602830b47d3a2d8269522c
Corina <photobycorina@gmail.com>
2025-04-06 19:44:23 +0200

    changes made to Task

--- greetings/models.py
+++ greetings/models.py
@@ -6,19 +6,34 @@ class Task(models.Model):
     STATUSES = [
         ('NEW', 'NEW'),
         ('IN PROGRESS', 'IN PROGRESS'),
-        ('RESOLVED', 'RESOLVED')
+        ('PENDING', 'PENDING'),
+        ('BLOCKED', 'BLOCKED'),
+        ('DONE', 'DONE'),
 
     ]
 
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
+    categories = models.ManyToManyField(Category, related_name="tasks",
+                                        help_text="Task categories")
     status = models.CharField(
         max_length=30,
         choices=STATUSES,
         default='NEW'
     )
+
     created_at = models.DateTimeField(auto_now_add=True)
     resolved_at = models.DateTimeField()
     deadline = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+
 
 
