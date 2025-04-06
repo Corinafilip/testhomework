2eaa1ea3dc63811f55cd9dfd59e2b6e5ae8044f2
Corina <photobycorina@gmail.com>
2025-04-06 19:26:12 +0200

    added first class Task

--- greetings/models.py
+++ greetings/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Task(models.Model):
+    STATUSES = [
+        ('NEW', 'NEW'),
+        ('IN PROGRESS', 'IN PROGRESS'),
+        ('RESOLVED', 'RESOLVED')
+
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    status = models.CharField(
+        max_length=30,
+        choices=STATUSES,
+        default='NEW'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    resolved_at = models.DateTimeField()
+    deadline = models.DateTimeField(null=True, blank=True)
+
+
