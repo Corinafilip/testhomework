62c9b7efce72dd8092216164abd4e3e9869b9eb6
Corina <photobycorina@gmail.com>
2025-04-13 08:54:40 +0200

    creating Task and Subtask objects

--- queries.py
+++ queries.py
@@ -10,4 +10,15 @@ django.setup()
 from greetings.models import  Task, SubTask, Category
 
 
+we_create_new_task = Task.objects.create(
+    title="Prepare presentation",
+    description="Prepare materials and slides for the presentation",
+    status="NEW",
+    deadline=timezone.now() + datetime.timedelta(days=3)
+)
 
+we_create_bulk_subtask = SubTask.objects.bulk_create([
+
+    SubTask(title="Gather Information", description="Find necessary info for the presentation", status="NEW", deadline=timezone.now() + datetime.timedelta(days=2)),
+    SubTask(title="Gather Information", description="Create presentation slides", status="NEW", deadline=timezone.now() + datetime.timedelta(days=1))
+])
