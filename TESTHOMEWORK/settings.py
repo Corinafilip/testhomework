2324451a51303bc3a8a1dd6ebba6551c50e1a20e
Corina <photobycorina@gmail.com>
2025-05-26 10:56:13 +0200

    swaggers added

--- TESTHOMEWORK/settings.py
+++ TESTHOMEWORK/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'greetings.apps.GreetingsConfig',
 
-
+    'drf_yasg',
 
 ]
 
--- TESTHOMEWORK/urls.py
+++ TESTHOMEWORK/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from greetings.views import *
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -26,6 +26,23 @@ from greetings.serializers.task_serializer import TaskCreateSerializer
 from greetings.views import greetings
 from greetings.views import create_task,  list_of_tasks, get_task_detail, new_tasks, in_progress_tasks, pending_tasks, blocked_tasks, done_tasks, overdue_tasks
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your Project API",
+      default_version='v1',
+      description="API documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="your.email@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny,],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -55,6 +72,10 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('mytasks/', UserTasksListView.as_view(), name='user-tasks'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
 
 
